3c3a4cd68004fa33c78ce9cef13bbb0791fb7b9a
Nick Welch <nwelchr@gmail.com>
2025-10-26 01:08:29 -0700

    sprites

---
+++ sprites.h
@@ -0,0 +1,15 @@
+/*
+ * Sprite drawing functions for Koetori M5StickC PLUS2
+ */
+
+#ifndef SPRITES_H
+#define SPRITES_H
+
+// Draw Bailey sprite (240x135) centered on screen
+void drawBailey();
+
+// Draw Gengar sprite (128x128) at specified position
+void drawGengar(int startX, int startY);
+
+#endif // SPRITES_H
+
