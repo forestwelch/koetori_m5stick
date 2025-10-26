6a2786703f284fba3863878901ac7d6a9374c239
Nick Welch <nwelchr@gmail.com>
2025-10-26 01:49:58 -0700

    recording

---
+++ recording.h
@@ -0,0 +1,18 @@
+/*
+ * Recording functions for Koetori M5StickC PLUS2
+ */
+
+#ifndef RECORDING_H
+#define RECORDING_H
+
+// Recording control
+void startRecording();
+void stopRecording();
+void cancelRecording();
+
+// Review and upload
+void displayReview();
+void saveAndUpload();
+
+#endif // RECORDING_H
+
